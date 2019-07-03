5a9cd569702eb3510590dc77c84197ca7dc10045
PauIGB <cwc-project@mail.ru>
2019-07-03 11:26:33 +0300

    test

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-require('@babel/register')
-require("dotenv").config()
+// require('@babel/register')
+// require("dotenv").config()
 
-exports = module.exports = require('./src')
+// exports = module.exports = require('./src')
+const express = require('express')
+const PORT = process.env.PORT || 5000
+
+express()
+ .get('/', (req, res) => {
+     res.send("hello")
+ })
+ .listen(PORT, () => console.log(`listen at: ${PORT}`))
