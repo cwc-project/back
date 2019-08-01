857b392eef642128d4616e2676a009c1cfe3e1dd
PauIGB <cwc-project@mail.ru>
2019-08-01 22:47:15 +0300

    morgan added

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     // 2 - error, 1 - warning, 0 - off
     'prettier/prettier': 'error', // mark prettier definitions as error
     'arrow-parens': [2, 'as-needed'], // for compatablility with prettier
-    'object-curly-newline': [2, { consistent: true }], // open prettier issue https://github.com/prettier/prettier/issues/2550    
+    'object-curly-newline': [2, { consistent: true }], // open prettier issue https://github.com/prettier/prettier/issues/2550
+    'no-unused-expressions': [2, { allowShortCircuit: true }],
   },
 };
